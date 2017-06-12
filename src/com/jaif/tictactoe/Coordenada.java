5b5bae8cc26c5e41acb82a62e79b74f676cb4788
Jaifman <jaifman@gmail.com>
2017-06-12 19:56:56 +0200

    Añadida nueva clase a efectos de OO

---
+++ src/com/jaif/tictactoe/Coordenada.java
@@ -0,0 +1,15 @@
+package com.jaif.tictactoe;
+
+public class Coordenada {
+
+	public void obtener() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public boolean valida() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
