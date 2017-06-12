b910323ccc55c525d784aa48f8ae6b8853d4e754
Jaifman <jaifman@gmail.com>
2017-06-12 20:19:19 +0200

    Falta implementar el método moverFicha()

--- src/com/jaif/tictactoe/Jugador.java
+++ src/com/jaif/tictactoe/Jugador.java
@@ -2,29 +2,37 @@ package com.jaif.tictactoe;
 
 public class Jugador {
 	
+	private char ficha;
+	
 	//Contructor con parámetro para establecer tipo de ficha del jugador
 	public Jugador(char ficha ) {
+		this.ficha = ficha;
+	}
+	
+	//Método para obtener la ficha
+	public char getFicha() {		
+		return ficha;
+	}
+	
+	//Método para establecer la ficha en el tablero
+	public void setFicha(Tablero tablero) {
+		Coordenada coordenada = new Coordenada();
+		do{
+			coordenada.obtener();
+		}while(!coordenada.valida() || tablero.ocupado(coordenada));
 		
+		tablero.setFicha(coordenada, ficha);
 	}
 	
 	//Método que mueve las fichas de los jugadores
-	public void setFicha(Tablero tablero) {
-		
-		
-	}
-
-	public void moverFicha(Tablero tablero) {
-		
+	public void moverFicha(Tablero tablero) {		
+				
 		
 	}
-
+	
+	//Método para mostrar por pantalla al vencedor
 	public void esVencedor() {		
-		
-	}
-
-	public char ficha() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+		System.out.println("Enhorabuena. El jugador con "+ficha+ " ha ganado");
+	}	
 
 }
