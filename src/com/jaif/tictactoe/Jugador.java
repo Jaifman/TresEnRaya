ca8741cd907687588fb5d27da985392adc577bba
Jaifman <jaifman@gmail.com>
2017-06-13 18:40:21 +0200

    Implementación de la clase Jugador

--- src/com/jaif/tictactoe/Jugador.java
+++ src/com/jaif/tictactoe/Jugador.java
@@ -2,7 +2,7 @@ package com.jaif.tictactoe;
 
 public class Jugador {
 	
-	private char ficha;
+	private char ficha;	
 	
 	//Contructor con parámetro para establecer tipo de ficha del jugador
 	public Jugador(char ficha ) {
@@ -16,18 +16,85 @@ public class Jugador {
 	
 	//Método para establecer la ficha en el tablero
 	public void setFicha(Tablero tablero) {
-		Coordenada coordenada = new Coordenada();
+		this.setFicha(tablero,null);
+	}
+	
+	public void setFicha(Tablero tablero,Coordenada coordenadaRepe){
+		System.out.println("Turno del jugador "+ficha+"\n");
+		tablero.setFicha(this.getCoordenadaPuestaValida(tablero, coordenadaRepe),ficha);
+	}
+	
+	private Coordenada getCoordenadaPuestaValida(Tablero tablero, Coordenada coordenadaRepe){
+		Coordenada resultado = new Coordenada();
+		String error = "";
 		do{
-			coordenada.obtener();
-		}while(!coordenada.valida() || tablero.ocupado(coordenada));
+			resultado.obtener();
+			error = this.errorPuesta(tablero, resultado, coordenadaRepe);
+			if(!error.equals("")){
+				System.out.println("Error: "+error+"\n");
+			}
+					
+		}while(!error.equals(""));
 		
-		tablero.setFicha(coordenada, ficha);
+		return resultado;
+		
+	}
+	
+	//Método que comprueba que no se pisen las fichas
+	private String errorPuesta(Tablero tablero, Coordenada coordenada, Coordenada coordenadaRepe){
+		
+		if(tablero.ocupado(coordenada)){
+			
+			return "Coordenada ya ocupada";
+			
+		}if (coordenadaRepe != null && coordenadaRepe.igual(coordenada)){
+			
+			return "No se puede colocar donde se retiró la ficha";
+			
+		}
+				
+		return "";
+		
 	}
 	
 	//Método que mueve las fichas de los jugadores
-	public void moverFicha(Tablero tablero) {		
-				
+	public void moverFicha(Tablero tablero) {
 		
+		System.out.println("Turno del jugador con ficha: "+ficha+"\n");		
+		Coordenada retirada = this.getCoordenadaRetiradaValida(tablero);
+		tablero.retirarFicha(retirada);
+		this.setFicha(tablero,retirada);			
+		
+	}
+	
+	private Coordenada getCoordenadaRetiradaValida(Tablero tablero){
+		Coordenada resultado = new Coordenada();
+		String error = "";
+		
+		do{
+			resultado.obtener();
+			error = this.errorRetirada(tablero, resultado);
+			
+			if(!error.equals("")){
+				
+				System.out.println("Error: "+error+"\n");
+				
+			}
+					
+		}while(!error.equals(""));
+		
+		return resultado;
+	}
+	
+	private String errorRetirada(Tablero tablero, Coordenada coordenada){
+		
+		if(!tablero.ocupado(coordenada,ficha)){
+			
+			return "Coordenada no ocupada en el tablero por una ficha"+ficha;
+			
+		}
+		
+		return "";
 	}
 	
 	//Método para mostrar por pantalla al vencedor
