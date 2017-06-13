20b94b316e9bc5dd7037b40924a6ae6ca3b51a8f
Jaifman <jaifman@gmail.com>
2017-06-13 17:16:23 +0200

    Refactorización + Error corregido

--- src/com/jaif/tictactoe/Turno.java
+++ src/com/jaif/tictactoe/Turno.java
@@ -7,22 +7,31 @@ public class Turno {
 	private int valor;
 	
 	//Establecemos el turno inicial de forma aleatoria
-	public Turno(){		
-		valor = new Random().nextInt(1);
+	public Turno(){	
+		
+		valor = new Random().nextInt(2);
+		
 	}
 	
 	//Método que devuelve a quien le toca jugar
 	public int esTurno() {
 		
 		return valor;
+		
 	}
+	
 	//Método que devuelve a quien no le toca jugar
-	public int noEsTurno() {		
+	public int noEsTurno() {	
+		
 		return (valor+1)%2;
+		
 	}	
+	
 	//Método para cambiar el turno
-	public void cambiar() {		
+	public void cambiar() {	
+		
 		this.noEsTurno();
+		
 	}	
 
 }
