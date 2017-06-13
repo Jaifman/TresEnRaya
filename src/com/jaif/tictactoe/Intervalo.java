8826cc41018b0d93ad2cb5941ae2cb7ddee008b8
Jaifman <jaifman@gmail.com>
2017-06-13 18:09:33 +0200

    Creación de la clase Intervalo de apoyo a la OO

---
+++ src/com/jaif/tictactoe/Intervalo.java
@@ -0,0 +1,30 @@
+package com.jaif.tictactoe;
+
+public class Intervalo {
+	
+	//Declaramos atributos
+	private double inferior;
+	private double superior;
+	
+	//Constructor con parámetros
+	public Intervalo(double inferior,double superior){
+		
+		this.inferior = inferior;
+		this.superior = superior;
+		
+	}
+	
+	//Método que comprueba si los números se encuentran en el intervalo
+	public boolean incluye(double valor){
+		
+		return inferior<=valor && valor <= superior;
+		
+	}
+	
+	
+	/*public boolean incluye(Intervalo intervalo){
+		
+		return this.incluye(intervalo.inferior) && this.incluye(intervalo.superior);
+		
+	}*/
+}
